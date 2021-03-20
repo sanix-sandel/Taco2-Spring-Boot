b2099d12ce979e4899e68eddfbc32e1737586006
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-03-20 20:51:12 +0300

    init

---
+++ src/main/java/com/example/Spring/Boot/Microservices/SpringBootMicroservicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.Spring.Boot.Microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesApplication.class, args);
+	}
+
+	
+}
